492bfc873c37cab5059aeea157cc68cb4de2080d
checkitsedo <56700394+checkitsedo@users.noreply.github.com>
2022-10-14 16:53:14 +0200

    Update ext_localconf.php

--- ext_localconf.php
+++ ext_localconf.php
@@ -1,11 +1,39 @@
 <?php
 defined('TYPO3') or die('Access denied.');
-/***************
- * Add default RTE configuration
- */
-$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['theme_velocasa'] = 'EXT:theme_velocasa/Configuration/RTE/Default.yaml';
 
 /***************
  * PageTS
  */
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig('<INCLUDE_TYPOSCRIPT: source="FILE:EXT:theme_velocasa/Configuration/TsConfig/Page/All.tsconfig">');
+
+
+/*
+ * ###########################
+ * Include default velocasa PageTS config
+ * ===========================
+ */
+
+// BE layouts
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:theme_velocasa/Configuration/TSconfig/Page/Mod/WebLayout/BackendLayouts.tsconfig'"
+);
+
+// RTE
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:theme_velocasa/Configuration/TSconfig/Page/RTE.tsconfig'"
+);
+
+// TCAdefaults
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:theme_velocasa/Configuration/TSconfig/Page/TCAdefaults.tsconfig'"
+);
+
+// TCEFORM
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:theme_velocasa/Configuration/TSconfig/Page/TCEFORM.tsconfig'"
+);
+
+// TCEMAIN
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+    "@import 'EXT:theme_velocasa/Configuration/TSconfig/Page/TCEMAIN.tsconfig'"
+);
